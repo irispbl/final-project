6ed8cf3f51c079b943d078e241cccc764574337c
ale <ale93chacon@gmail.com>
2022-12-01 18:02:11 +0100

    almost-ale

--- javascript/thirdpage.js
+++ javascript/thirdpage.js
@@ -1,5 +1,11 @@
-function validateForm() {
-    /*let correo = document.forms["myForm"]["fmail"].value;*/
+const boton = document.querySelector(".submit");
+boton.addEventListener("click", (e)=>{
+    e.preventDefault();
+    validateName();
+    validateEmail();
+});
+function validateName() {
+    
     let x = document.forms["myForm"]["fname"].value;
     if (x == "") {
       alert("Name must be filled out");
@@ -9,7 +15,13 @@ function validateForm() {
         alert("You cannot be Ironhack, because I am Ironhack.");
         return false;
     }
-
     return true;
 
+}
+function validateEmail(){
+    const regEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+    const query = document.querySelector("#correo").value;
+if(!regEmail.test(query)){
+    alert("Email not valid");
+}
 }
