3b0276a62acbce09b7be7c1f6e38c3d225352b2f
ale <ale93chacon@gmail.com>
2022-12-01 11:32:35 +0100

    java1-ale

--- javascript/index.js
+++ javascript/index.js
@@ -4,21 +4,38 @@ async function fetchProjects(){
     const response = await fetch(api_url);
     const data= await response.json();
     const sortedProjects = data.sort(function(a, b){
-        if (a.uuid > b.uuid){
+        if (Math.random() > 0.5){
             return -1;
         }
-        if (a.uiid < b.uuid){
+        else{
             return 1;
         }
-        return 0;
-    });
+        });
+        
+    
     const newstr = sortedProjects.slice(0, 3);
-    console.log(newstr);
-    console.log("hola");
-}
+    const divMarginProject = document.getElementsByClassName("margin-project");
+    
+    
+   newstr.forEach(function(project, i){
+    divMarginProject[i].querySelector("img").setAttribute("src", newstr[i].image);
+    divMarginProject[i].querySelector("h3").innerHTML = newstr[i].name;
+    divMarginProject[i].querySelector("p").innerHTML = newstr[i].description;
+    
+   });
 
-/* const myButton = document.getElementById("button-project");
-myButton.addEventListener("click", function(event){
-    fetchProjects();
+}
+fetchProjects();
+window.addEventListener("load",async function(){
+    /*const post = await fetchProjects();
+    console.log(post);*/
+
+
+
 });
-*/
+
+
+/*const myButton = document.getElementById("button-project");
+myButton.addEventListener("load", function(event){
+    fetchProjects();
+});*/
